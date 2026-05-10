050b9183c8378c537d525fe3b02fe41cf4dec785
tobiasw225 <tobiasw225@gmail.com>
2026-05-10 19:06:59 +0200

    test: remove dead fixtures and unused dependencies
    Remove fixtures that reference non-existent data files:
    default_txt_file_name, original_txt_file, original_byte_file,
    original_hebrew_file_name, test_utf_8_text, random_wiki_articles.
    
    Remove get_random_wiki_articles, random_utf_word, and the
    unused requests dependency from pyproject.toml.

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,7 +1,4 @@
-import random
 from typing import Iterable, List
-
-import requests
 
 from python_aes.text_to_number_conversion import generate_nonce
 
@@ -11,54 +8,9 @@ def assert_blocks_equal(block_a: Iterable, block_b: Iterable):
         assert a == b
 
 
-def get_random_wiki_articles(n: int):
-    """
-
-    :param n:
-    :return:
-    """
-    session = requests.Session()
-    url = "https://en.wikipedia.org/w/api.php"
-    res = session.get(
-        url=url,
-        params={
-            "action": "query",
-            "format": "json",
-            "list": "random",
-            "rnlimit": f"{n}",
-        },
-    )
-    data = res.json()
-    articles = data["query"]["random"]
-    # crawl actual articles
-    res = session.get(
-        url=url,
-        params={
-            "action": "query",
-            "format": "json",
-            "prop": "extracts",
-            "exlimit": "max",
-            "explaintext": "true",
-            "titles": "|".join(r["title"] for r in articles),
-        },
-    )
-    data = res.json()
-    for article in data["query"]["pages"].values():
-        # some articles have no text.
-        yield f"{article['title']}\n{article.get('extract', '')}"
-
-
 def sample_nonce(block_size: int) -> List[int]:
     nonce = [0] * block_size
     _nonce = generate_nonce(d_type=int, block_size=block_size // 2)
     for i, n in enumerate(_nonce):
         nonce[i] = n
     return nonce
-
-
-def random_utf_word(k: int) -> str:
-    return "".join(
-        random.choices(
-            tuple(chr(i) for i in range(32, 100000) if chr(i).isprintable()), k=k
-        )
-    )
